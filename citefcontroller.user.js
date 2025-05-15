66da222c50e8b5ee051ebd4b84558c138cdf062a
Julien Cassagne <julien.cassagne@partner.ibm.com>
2025-05-15 11:01:41 -0400

    Add VNC handler

--- citefcontroller.user.js
+++ citefcontroller.user.js
@@ -2,8 +2,9 @@
 // @name        CITEF Controller
 // @namespace   uOttawa-IBM Cyber Range script
 // @match       https://citefplus.griseo.ca/*
-// @grant       window.focus
-// @version     1.0
+// @match       http://10.20.1.11:8080/*
+// @grant       none
+// @version     1.1
 // @author      Julien Cassagne
 // @description 2025-05-10, 10:28:14 p.m.
 // @downloadURL https://raw.githubusercontent.com/UOttawa-Cyber-Range-Scenarios/userscripts/refs/heads/main/citefcontroller.user.js
@@ -13,14 +14,16 @@
 // (e.g mat-input-0 can become mat-input-2 after accessing other pages)
 
 const routeHandlers = {
-  '/login': handlerLogin,
-  '/password-reset': handlerPasswordReset
+  'login': handlerLogin,
+  'password-reset': handlerRedirectScenario,
+  'home': handlerRedirectScenario,
+  'scenario-vnc': handlerScenarioVnc
 };
 
 async function CITEFController() {
   await new Promise(resolve => setTimeout(resolve, 1000)); // wait util js gets executed
 
-  const route = window.location.pathname;
+  const route = window.location.pathname.split('/')[1] || undefined;
   console.warn(`CITEFController: Starting on ${route}`);
 
   const handler = routeHandlers[route];
@@ -43,10 +46,14 @@ async function handlerLogin() {
   submitButton.click();
 }
 
-async function handlerPasswordReset() {
+async function handlerRedirectScenario() {
   location.href = '/scenario';
 }
 
+async function handlerScenarioVnc() {
+  const fullscreen = document.getElementsByClassName("vnc-console-mat-icon-button")[0];
+  fullscreen.click()
+}
 
 
 
